b9f35fb5c0a1a22d0342ae6654e9aa7ec45e3e12
Yevhen Stepanenko <yevhenstepanenko@Yevhens-MacBook-Pro.local>
2022-01-27 21:19:23 +0200

    First cypress test

---
+++ first_test_spec.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+
+describe('User should be able to', () => {
+    let user;
+    before(function () {
+        cy.visit('https://react-redux.realworld.io/#/');
+    
+        cy.task("freshUser").then((freshUser) => {
+            user = freshUser;
+        });
+    });
+
+    it('register', () => {
+        cy.get(':nth-child(3) > .nav-link').click();
+        cy.get(':nth-child(1) > .form-control').type(user.username).should('have.value', user.username);
+        cy.get(':nth-child(2) > .form-control').type(user.email).should('have.value', user.email);
+        cy.get(':nth-child(3) > .form-control').type(user.password).should('have.value', user.password);
+        cy.get('.btn').click();
+        cy.get(':nth-child(4) > .nav-link').should('contain', `${user.username}`);
+        cy.get(':nth-child(3) > .nav-link').click();
+        cy.get('.btn-outline-danger').click();
+      });
+
+    it('login with registered credentials', () => {
+      cy.get(':nth-child(2) > .nav-link').click();
+      cy.get(':nth-child(1) > .form-control').type('sikasu@mailinator.com');
+      cy.get(':nth-child(2) > .form-control').type('Password12345.');
+      cy.get('.btn').click();
+      cy.get(':nth-child(4) > .nav-link').should('contain', 'Dicta');
+    });
+  })
